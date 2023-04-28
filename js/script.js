30d0ac117abb7bb1c00cd9759e28b354c915dfd7
Collin R <collin.codes.hard@gmail.com>
2023-04-28 13:03:52 -0500

    Created function to display user info

--- js/script.js
+++ js/script.js
@@ -2,3 +2,29 @@
 const overview = document.querySelector(".overview");
 //GitHub username
 const username = "Collin-cr8"
+//Create async function to fetch gitHub profile info using GitHub API
+const getGitHub = async function () {
+    const userInfo = await fetch(`https://api.github.com/users/${username}`);
+    const gitProfileInfo = await userInfo.json();
+    console.log(gitProfileInfo);
+    
+
+};
+
+getGitHub();
+
+//Create function to display fetched github info
+const displayUserInfo = function(gitProfileInfo) {
+    const div = document.createElement(div);
+    div.classList.add("user-info");
+    div.innerHTML = `<figure>
+    <img alt = "user avatar" src=${getGitHub.avatar_url} />
+    </figure>
+  <div>
+    <p><strong>Name:</strong> ${getGitHub.name}</p>
+    <p><strong>Bio:</strong> ${getGitHub.bio}</p>
+    <p><strong>Location:</strong> ${getGitHub.location}</p>
+    <p><strong>Number of public repos:</strong> ${getGitHub.public_repos}</p>
+  </div> `
+  overview.append(div);
+};
